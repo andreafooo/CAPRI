0626523dd019bf6fefe93903008116d4486b11c7
andreaforster <97340144+chicadelica@users.noreply.github.com>
2024-11-12 13:43:11 +0100

    adapt config

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ isInteractive = True  # Should be false when the Terminal is not available
 # Default Parameters
 topK = 10  # Top-k items to evaluate (default: 10)
 limitUsers = -1  # Limit the number of users (default: -1)
-listLimit = 50  # Limit the length of recommendation list (default: 10)
+listLimit = 150  # Limit the length of recommendation list (default: 10)
 activeUsersPercentage = [5, 20]  # Calculate [n] percents of users as active
 
 # Default Parameters (non-interactive)
@@ -33,10 +33,10 @@ datasets = {
     "Gowalla": ["Geographical", "Social", "Temporal", "Interaction"],
     "Yelp": ["Geographical", "Social", "Temporal", "Categorical", "Interaction"],
     "Foursquare": ["Geographical", "Social", "Temporal", "Interaction"],
-    "Gowalla_Sample": ["Geographical", "Social", "Temporal", "Interaction"],
-    "Brightkite_Sample": ["Geographical", "Social", "Temporal", "Interaction"],
+    "Gowalla_Sample": ["Geographical", "Temporal", "Interaction"],
+    "Brightkite_Sample": ["Geographical", "Temporal", "Interaction"],
     "Yelp_Sample": ["Geographical", "Temporal", "Interaction"],
-    "Foursquare_TKY": ["Geographical", "Temporal", "Interaction"],
+    "Foursquaretky_Sample": ["Geographical", "Temporal", "Interaction"],
     "Snowcard_Sample": ["Geographical", "Temporal", "Interaction"],
 }
 
@@ -45,7 +45,7 @@ datasets = {
 fusions = ["Product", "Sum"]
 
 # List of evaluation metrics
-evaluationMetrics = ["Precision", "Recall", "mAP", "NDCG", "listDiversity"]
+evaluationMetrics = ["Precision", "Recall", "mAP", "NDCG"]
 
 # Models Dictionaries
 GeoSoCaDict = {"alpha": 0.5}
